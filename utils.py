f4ffe29df07e28f80578ea2d3b7f0b53facaa623
NamDT-146 <namdotuan146@gmail.com>
2024-12-12 13:12:21 +0700

    update utils

---
+++ utils.py
@@ -0,0 +1,88 @@
+def read_input(from_file=False, file_path=None):
+    """
+    Reads input for the TSP with Time Windows problem.
+    
+    Args:
+        from_file (bool): Whether to read input from a file.
+        file_path (str): Path to the input file (if from_file is True).
+
+    Returns:
+        N (int): Number of nodes (customers + depot).
+        time_windows (list of tuples): List of (e(i), l(i), d(i)) for each node.
+        travel_time (list of lists): Matrix of travel times t(i, j).
+    """
+    if from_file and file_path:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        
+        # Read number of nodes
+        N = int(lines[0].strip())
+
+        # Read time window and service time for each node
+        time_windows = []
+        for i in range(1, N + 1):
+            e, l, d = map(int, lines[i].strip().split())
+            time_windows.append((e, l, d))
+
+        # Read the travel time matrix
+        travel_time = []
+        for i in range(N + 1, len(lines)):
+            row = list(map(int, lines[i].strip().split()))
+            travel_time.append(row)
+
+    else:
+        # Read number of nodes
+        N = int(input())
+
+        # Read time window and service time for each node
+        time_windows = []
+        for _ in range(N):
+            e, l, d = map(int, input().split())
+            time_windows.append((e, l, d))
+
+        # Read the travel time matrix
+        travel_time = []
+        for _ in range(N + 1):  # N+1 because of depot (node 0)
+            row = list(map(int, input().split()))
+            travel_time.append(row)
+
+    return N, time_windows, travel_time
+
+ 
+
+def evaluate(solution, time_windows, travel_time):
+    """
+    Evaluates a given solution for the TSP with Time Windows problem.
+
+    Args:
+        solution (list): A permutation of nodes representing the delivery route.
+        time_windows (list of tuples): Time windows (e(i), l(i)) and service durations (d(i)) for each node.
+        travel_time (list of lists): Matrix of travel times t(i, j).
+
+    Returns:
+        tuple:
+            bool: True if the solution is valid (meets all time window constraints), False otherwise.
+            int: Total time taken for the route if valid, or -1 if invalid.
+    """
+
+    total_time = 0
+    present_position = 0
+    for next_position in solution:
+        early_TW, late_TW, dur = time_windows[next_position]
+        if next_position == 0: 
+            total_time = max(total_time, early_TW) + dur #Ready to go
+            continue
+
+        total_time += travel_time[present_position][next_position]
+        total_time = max(total_time, early_TW)
+
+        if total_time <= late_TW:   
+            total_time += dur
+        else: 
+            return False, -1
+
+        present_position = next_position
+
+    return True, total_time + travel_time[present_position][0]
+    
+
